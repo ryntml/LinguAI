001bdde8201bfe4cd4934c5227b11987137dbb9c
ryntml <ryntml9@hotmail.com>
2024-08-08 12:16:11 +0300

    Web Scraping kodu eklendi.

--- scraping.py
+++ scraping.py
@@ -1,34 +1,37 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+import pandas as pd
+import os
+import time
 
 # ChromeDriver'ı başlat
-chrome_options = Options()
-chrome_options.add_argument("--headless")  # Tarayıcıyı arka planda çalıştır
-service = Service('C:/Users/ryntm/Desktop/chromedriver.exe')
+#service = Service(ChromeDriverManager().install())
+service = Service('C:/Users/ryntm/Desktop/chromedriver.exe')  # Chromedriver'ın yolu 
 driver = webdriver.Chrome(service=service)
-# Web sayfasının URL'si
-url = 'https://www.sikayetvar.com/ulker'
-
-# Sayfayı aç
+url = 'https://www.sikayetvar.com/teksut'  # Yorumların bulunduğu sayfanın URL'si
 driver.get(url)
+time.sleep(20)
 
-# Sayfanın dinamik olarak yüklenmesini bekle
-try:
-    # CSS seçicisini güncellemen gerekebilir
-    review_elements = WebDriverWait(driver, 10).until(
-        EC.presence_of_all_elements_located((By.CSS_SELECTOR, 'a.complaint-description'))
-    )
+# Yorumları içeren elementleri bulmak için
+#comments = driver.find_elements(By.CSS_SELECTOR, 'div.comment-text p') # html'deki div adı neyse onu yazıyoruz (burda comment-text), sonra p de türü. 
+#comments = driver.find_elements(By.CSS_SELECTOR, 'div.hermes-Comments-module-kV6VmHxTOAz2NZN1JIxw span') # türü span 
+comments = driver.find_elements(By.CSS_SELECTOR, 'a.complaint-description') # türü a
+comments_list = [comment.text for comment in comments]
+driver.quit()
+new_df = pd.DataFrame(comments_list, columns=['text'])
+file_path = 'Yorumlar.xlsx'
+if os.path.exists(file_path):
+    # Var olan dosyayı oku
+    existing_df = pd.read_excel(file_path)
+    # Yeni verilerle birleştir
+    combined_df = pd.concat([existing_df, new_df], ignore_index=True)
+else:
+    # Dosya yoksa sadece yeni verileri kullan
+    combined_df = new_df
 
-    # İnceleme metinlerini çıkart
-    review_content = [review.get_attribute('textContent').strip() for review in review_elements]
+# Birleştirilmiş DataFrame'i Excel dosyasına kaydedin
+combined_df.to_excel(file_path, index=False)
 
-    print(review_content)
-
-finally:
-    # Tarayıcıyı kapat
-    driver.quit()
+print("Yorumlar başarıyla '{}' dosyasına kaydedildi.".format(file_path))
